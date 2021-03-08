4d82e7839bd07d59fb8c9bd878f649c68bc52541
Victor Oliveira <victor5171@gmail.com>
2021-03-08 19:29:46 +0100

    Updated Gradle

--- buildSrc/src/main/java/SdkConfig.kt
+++ buildSrc/src/main/java/SdkConfig.kt
@@ -1,7 +1,10 @@
+import org.gradle.api.JavaVersion
+
 object SdkConfig {
     const val COMPILE_SDK_VERSION = 30
     const val MIN_SDK_VERSION = 22
     const val TARGET_SDK_VERSION = 30
     const val BUILD_TOOLS_VERSION = "30.0.2"
     const val ANDROID_TOOLS_VERSION = "4.0.1"
+    val JAVA_VERSION = JavaVersion.VERSION_1_8
 }
